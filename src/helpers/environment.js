92d869a56e92c31ddf9fcf1b6d06ef0ad7b35d29
Perla Brown <62082149+PerlaBrownCreative@users.noreply.github.com>
2021-05-08 14:03:49 -0400

    message

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,7 +4,7 @@ switch (window.location.hostname) {
     case 'localhost' || '127.0.0.1':
         APIURL = 'http://localhost:4000';
         break;
-        case "pbcoapparelclient.herokuapp.com/":
+        case "pbcoapparelclient.herokuapp.com":
             APIURL= 'https://pbcoapparel.herokuapp.com'
 }
 
